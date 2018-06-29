78a30c6d15333a9662629a191e7bf66f8dd56cf8
Schlueter <SchlueterPro@gmail.com>
2018-06-28 21:04:21 -0700

    put new ideas in javascript psuedocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// Generate a random number at the beginning of each game. (probably 1-20)
+
+// Make the gems clickable.
+
+// Assign a random value to each gem. (probably 1-5)
+
+// When a gem is clicked, push the value to the overall score and continue to do so with each click
+
+// Create if statement for a "win" to reset the game and update the wins column.
+
+// Create if statement for a "loss" to reset the game and update the losses column.
+
+// maybe display:none for the gem values.
+
+
+
+
+var randomNumber = Math.floor(Math.random) * 20

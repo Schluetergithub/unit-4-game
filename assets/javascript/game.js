5febff4553f1f78f8fe5f8b2b77ef8ead4147dca
Schlueter <SchlueterPro@gmail.com>
2018-07-01 10:25:51 -0700

    added bootstrap to HTML. Assigned values to gems and pushed them to overall score onclick function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,15 +15,31 @@
 
 $(document).ready(function() {
 
-var randomNumber = Math.floor(Math.random() * 20);
+var randomNumber = Math.floor(Math.random() * 50);
+console.log(randomNumber);
 
-var gem_value = Math.floor(Math.random() * 20);
+// var gem_value = Math.floor(Math.random() * 20);
 
-var total_score = [];
+var total_score = 0;
 
 var wins = 0;
 
 var losses = 0;
+
+var button1 = Math.floor(Math.random() * 13);
+console.log(button1)
+
+var button2 = Math.floor(Math.random() * 13);
+console.log(button2)
+
+var button3 = Math.floor(Math.random() * 13);
+console.log(button3)
+
+var button4 = Math.floor(Math.random() * 13);
+console.log(button4)
+
+
+// var goal = "";
 
 var reset = function (){
     document.getElementById("Wins").innerHTML = wins;
@@ -31,54 +47,84 @@ var reset = function (){
     document.getElementById("Score").innerHTML = total_score;
 }
 
+// push random number to the empty "goal" span
+
+$("#goal").html(randomNumber);
+
+
+
+
+
+
 // onclick function for gems to be assigned random value
+// add the gem value to total score.
+
 $("#gem1").on("click", function() {
 
-    gem_value = Math.floor(Math.random() * 20)
-}
+    total_score += button1;
+
+
+    $("#game-score").html(total_score)
+    // gem_value = Math.floor(Math.random() * 20)
+    // console.log(gem_value)
+})
 
 $("#gem2").on("click", function() {
 
-    gem_value = Math.floor(Math.random() * 20)
-}
+    total_score += button2;
+
+    $("#game-score").html(total_score)
+//     gem_value = Math.floor(Math.random() * 20)
+ })
 
 $("#gem3").on("click", function() {
 
-    gem_value = Math.floor(Math.random() * 20)
-}
+    total_score += button3;
+
+    $("#game-score").html(total_score)
+    // gem_value = Math.floor(Math.random() * 20)
+})
 
 $("#gem4").on("click", function() {
 
-    gem_value = Math.floor(Math.random() * 20)
-}
+    total_score += button4;
+    
+    $("#game-score").html(total_score)
 
-
-
-
-
-
+//     gem_value = Math.floor(Math.random() * 20)
+ })
 
 
 
 
 
 // onclick function for gem buttons to push to scoreboard
-$(".to_scoreboard").on("click", function() {
-
-    $("#gem_value").prepend("<br><hr>" + to_scoreboard);
 
 
+// $(".to_scoreboard").on("click", function() {
+
+//     total_score.push(gem_value);
 
 
-}
+//     // $("#gem_value").prepend("<br><hr>" + game_score);
+
+// }
 
 
 // if - win scenario
-if (total_score === randomNumber) {
-    wins++
+if(total_score === randomNumber) {
+    wins++;
     alert("Congratulation! You collected the EXACT value! Let's play again!");
+    randomNumber = Math.floor(Math.random() * 50);
 }
 
+if(total_score > randomNumber) {
+    losses++;
+    alert("Almost! Try again!");
+    randomNumber = Math.floor(Math.random() * 50);
+}
+
+reset();
 
 
 
